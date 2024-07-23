7f10d6fef929c1296fffea37dfc17d4dfc5c0133
Drew Phillips <dbphilli@dphe.state.co.us>
2024-07-23 10:00:14 -0600

    reorganized dir

--- data_engineering/ex_1_download/ex_1_download.py
+++ data_engineering/ex_1_download/ex_1_download.py
@@ -3,6 +3,15 @@ import urllib.request
 import zipfile
 import os
 
+"""
+TODO
+---
+
+- create download_file()
+- create process_file()
+
+"""
+
 DOWNLOAD_DIR = 'downloads'
 
 URLS = [
@@ -15,7 +24,22 @@ URLS = [
       "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
       ]
 
-def process_file(url):
+def is_valid_url(url):
+    """Check that URL is valid before processing"""
+    
+    try:
+        response = urllib.request.urlopen(url)
+        return response.status == 200
+    
+    except urllib.error.URLError as e:
+        return False
+    
+    except urllib.error.HTTPError as e:
+        return False
+    
+
+def create_paths(url):
+    """Make paths or path-like objects from url (and ZIP file name)"""
 
     # Make destination folder to hold ZIP and member files
     destination_folder = os.path.join(os.getcwd(), DOWNLOAD_DIR)
@@ -26,18 +50,38 @@ def process_file(url):
     # Point to where ZIP file will be saved (incl filename)
     destination_filename = os.path.join(destination_folder, zip_filename)
 
+    return destination_filename, destination_folder
+
+
+
+def download_file(url, destination_filename):
+    """Create paths and retreive file from valid url"""
+
     try:
-        # Download file
         urllib.request.urlretrieve(url, destination_filename)
 
-        # Extract all members of zip file
-        with zipfile.ZipFile(destination_filename, mode="r") as archive:
-            archive.extractall(destination_folder)
-
-        os.remove(destination_filename)
-    
     except Exception as e:
-        print(f"{e}")
+        print(f"Error in downloading file from {url}")
+
+
+def extract_and_delete(destination_filename, destination_folder):
+    """Extracts the csv from the ZIP file"""
+
+    with zipfile.ZipFile(destination_filename, mode="r") as archive:
+        archive.extractall(destination_folder)
+
+    os.remove(destination_filename)
+
+
+def process_file(url):
+
+    if is_valid_url(url):
+       
+       destination_filename, destination_folder = create_paths(url)
+
+       download_file(url, destination_filename)
+
+       extract_and_delete(destination_filename, destination_folder)
 
 
 if __name__ == "__main__":
